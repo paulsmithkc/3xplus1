273d7bdcbd58b9b1ba0de8f0cd70c4bfe7c40ef7
Paul Smith <paulsmithkc@gmail.com>
2021-07-31 11:42:28 -0500

    improving how chains are stored to reduce memory usage

--- backtrack.js
+++ backtrack.js
@@ -1,22 +1,39 @@
 const start = BigInt(process.argv[2] || '4');
 const limit = parseInt(process.argv[3] || '10');
-const queue = [[start]];
+const queue = [{cur: start, length: 0}];
+const map = new Map();
 console.log(`tracing back sequences ending with ${start}\n`);
 
 while (queue.length) {
   const chain = queue.shift();
-  const cur = chain.at(-1);
+  const cur = chain.cur;
+
+  if (!map.has(cur)) {
+    map.set(cur, chain);
+  } else if (map.get(cur).length > chain.length) { 
+    map.set(cur, chain);
+  }
+
   if (cur <= 1 || chain.length >= limit) {
     // stop exploring this chain
-    console.log(chain.join(', '));
   } else {
     const a = cur * 2n;
     const b = (cur - 1n) / 3n;
     if (a % 2n === 0n) {
-      queue.push([...chain, a]);
+      queue.push({cur: a, prev: cur, length: chain.length + 1});
     }
     if (b % 2n !== 0n) {
-      queue.push([...chain, b]);
+      queue.push({cur: b, prev: cur, length: chain.length + 1});
     }
   }
 }
+
+// for (const chain of map.values()) {
+//   console.log(`${chain.cur} => ${start} in ${chain.length} steps.`);
+// }
+
+const list = [];
+for (const cur of map.keys()) {
+  list.push(cur);
+}
+console.log(list.join(', '), '\n');
