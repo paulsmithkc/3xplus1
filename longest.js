b27b42b0ccd7b89c87119d737c292ffa2f70ebf3
Paul Smith <paulsmithkc@gmail.com>
2021-07-31 10:42:58 -0500

    renaming limit

--- longest.js
+++ longest.js
@@ -1,13 +1,13 @@
 const start = BigInt(process.argv[2] || '4');
-const count = BigInt(process.argv[3] || '1000');
-console.log(`finding ${count*2n}+ number sequence that ends with ${start}`);
+const limit = BigInt(process.argv[3] || '1000');
+console.log(`finding ${limit*2n}+ number sequence that ends with ${start}`);
 
 // 1 just loops back to one
 if (start === 1n) return;
 
 // iterate looking for the previous number
 let x = start;
-for (let i = 0n; i < count; ++i) {
+for (let i = 0n; i < limit; ++i) {
   x = (4n * x + 1n) / 3n;
 }
 
